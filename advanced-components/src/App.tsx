d1e907c88b949fc2a257c22ed8a95b18df97f0d1
charo <yuya1charo@gmail.com>
2024-06-24 07:52:33 +0900

    forward Ref, type unknown, FormData etc

--- advanced-components/src/App.tsx
+++ advanced-components/src/App.tsx
@@ -1,13 +1,26 @@
 import Button from "./components/Button";
 import Input from "./components/Input";
 import Container from "./components/Container";
+import { useRef, forwardRef } from "react";
+import Form from "./components/Form";
 
 export default function App() {
+  const input = useRef<HTMLInputElement>(null);
+
+  const handleSave = (data: unknown) => {
+    const extractedData = data as { name: string; age: string };
+    console.log(extractedData);
+  };
+
   return (
     <main>
-      <Input id="name" label="Your name" />
-      <Input id="age" label="Your age" type="number" />
-      <Container as={Button}>Click me</Container>
+      <Form onSave={handleSave}>
+        <Input id="name" label="Your name" ref={input} />
+        <Input id="age" label="Your age" type="number" />
+        <p>
+          <Button>Click me</Button>
+        </p>
+      </Form>
     </main>
   );
 }
